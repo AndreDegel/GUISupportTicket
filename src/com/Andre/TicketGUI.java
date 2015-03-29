303c5fb31d08d90fd85549c1746a392916692490
Andre <dego89@gmx.net>
2015-03-29 15:59:29 -0500

    Final commit with working file writing and reading

--- src/com/Andre/TicketGUI.java
+++ src/com/Andre/TicketGUI.java
@@ -1,6 +1,5 @@
 package com.Andre;
 
-import javax.imageio.IIOException;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -8,9 +7,10 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -33,14 +33,14 @@ public class TicketGUI extends JFrame{
 
     DefaultListModel<Ticket> ticketListModel;
     ArrayList<Ticket> resolvedTickets = new ArrayList<Ticket>(); //new arraylist to store resolved tickets and later put to txt file
-    ArrayList<Ticket> openTickets = new ArrayList<Ticket>(); //new arraylist to store open tickets and later put to txt file
-    public TicketGUI() {
+    ArrayList<Ticket> openTicketsList = new ArrayList<Ticket>(); //new arraylist to store open tickets and later put to txt file
+    public TicketGUI() throws IOException{
         super("List of Support Tickets");
         setContentPane(rootPanel);
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        setSize(new Dimension(400, 500));
+        setSize(new Dimension(1000, 500));
 
         final String one = "1";
         final String two = "2";
@@ -57,6 +57,45 @@ public class TicketGUI extends JFrame{
         ticketList.setModel(ticketListModel);
         ticketList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
+        try {
+            BufferedReader openTickets = new BufferedReader(new FileReader("open_tickets.txt"));
+            String line = openTickets.readLine();
+
+            while (line != null) {
+                String words[] = line.split("[,:]");
+                line = openTickets.readLine();
+                String desc = words[3];
+                int prio = Integer.parseInt(words[5].trim());
+                String rep = words[7];
+                //create a string of the read out date
+                //(could not parse it like this)
+                String dateString = (words[9] + ":" + words[10] + ":" + words[11]).trim();
+                //split the new date string
+                String dateParse[] = dateString.split(" ");
+                //give it a new format to fit the formatter
+                dateString = dateParse[2] + " " + dateParse[1] + " " +
+                        dateParse[5] + " " + dateParse[3];
+                //create simple format to parse
+                DateFormat formatter = new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
+                //try to parse string into date
+                try {
+                    Date date = formatter.parse(dateString);
+                    //once date is parses create new ticket and put into queue
+                    Ticket queue = new Ticket(desc, prio, rep, date);
+                    openTicketsList.add(queue);
+                    TicketGUI.this.ticketListModel.addElement(queue);
+                }
+                //catch exception
+                catch (ParseException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+        //ignore if it doesn't exist
+        catch (FileNotFoundException e){
+
+        }
+
 
         addTicketButton.addActionListener(new ActionListener() {
             @Override
@@ -68,26 +107,46 @@ public class TicketGUI extends JFrame{
                 int priorityAsInt = Integer.parseInt(priority);
                 Ticket newTicket = new Ticket(issue, priorityAsInt, reporter, dateReported);
                 TicketGUI.this.ticketListModel.addElement(newTicket);
-                openTickets.add(newTicket);
-                System.out.println(openTickets.toString());
+                openTicketsList.add(newTicket);
+                System.out.println(openTicketsList.toString());
             }
         });
         deleteTicketButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: not working yet
-                Ticket toDelete = TicketGUI.this.ticketList.getSelectedValue();
-                txtResolved.setText("");
-                txtResolved.setEditable(true);
-                try {
-                    toDelete.setResolution(txtResolved.getText());
+                //if a ticket is selected to be resolved
+                if (!TicketGUI.this.ticketList.isSelectionEmpty()) {
+                    Ticket toDelete = TicketGUI.this.ticketList.getSelectedValue();
 
-                    resolvedTickets.add(toDelete);
+                    Date dateResolved = new Date(); //Default constructor creates date with current date/time
 
+                    //open input dilog to resolve the ticket
+                    String s = JOptionPane.showInputDialog(
+                            null,
+                            "How did you resolve the issue:",
+                            "Resolve Ticket",
+                            JOptionPane.PLAIN_MESSAGE);
+
+                    //If a string was returned, set resolution and date, add to resolved list, and delete ticket from list
+                    //if not ignore it
+                    if ((s != null) && (s.length() > 0)) {
+                        openTicketsList.remove(toDelete);
+                        txtResolved.setText(s);
+                        toDelete.setResolvedDate(dateResolved);
+                        toDelete.setResolution(txtResolved.getText());
+                        resolvedTickets.add(toDelete);
+                        TicketGUI.this.ticketListModel.removeElement(toDelete);
+                        clearTxt();
+                        txtIssue.setText("");
+                        txtReportedBy.setText("");
+                        return;
+                    }
                 }
-                catch (NullPointerException npe) {
-                    JOptionPane.showMessageDialog(TicketGUI.this, "Enter how you resolved the issue");
-                    return;
+                //show error message if no ticket is selected
+                else {
+                    JOptionPane.showMessageDialog(null,
+                            "Please select a ticket to be resolved!",
+                            "Error", JOptionPane.ERROR_MESSAGE);
                 }
 
             }
@@ -121,7 +180,6 @@ public class TicketGUI extends JFrame{
             @Override
             public void insertUpdate(DocumentEvent e) {
                 clearTxt();
-
             }
 
             @Override
@@ -141,21 +199,28 @@ public class TicketGUI extends JFrame{
         saveQuitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(openTickets.toString());
-                Date dateResolved = new Date(); //Default constructor creates date with current date/time
-                //Open a new buffered writer to write the resolved files
+                DateFormat df = new SimpleDateFormat("MMM_dd_yyyy");
+                // Get the date today using Date object.
+                Date today = new Date();
+                // Using DateFormat format method we can create a string
+                // representation of a date with the defined format
+                // to use to create todays resolve file.
+                String reportDate = df.format(today);
+
+                //Open a new buffered writers to write the resolved and open tickets to files
                 try {
+                    BufferedWriter todayResolved = new BufferedWriter(new FileWriter("Resolved tickets_as_of_" +
+                            reportDate + ".txt"));
                     BufferedWriter open = new BufferedWriter(new FileWriter("open_tickets.txt"));
-                    //BufferedWriter todayResolved = new BufferedWriter(new FileWriter("Resolve tickets_as_of_" +
-                      //      dateResolved + ".txt"));
-                    for (Ticket openTicket : openTickets) {
+                    for (Ticket openTicket : openTicketsList) {
                         open.write(openTicket.toString() + "\n");
                     }
-                    //for (Ticket resolved : resolvedTickets){
-                      //  todayResolved.write(resolved.toString() + "\n");
-                    //}
+                    for (Ticket resolved : resolvedTickets) {
+                        todayResolved.write(resolved.toString() + "\n");
+                    }
+
+                    todayResolved.close();
                     open.close();
-                    //todayResolved.close();
                 }
                 catch (IOException iex) {
                     System.out.println(iex);
@@ -170,7 +235,7 @@ public class TicketGUI extends JFrame{
     //Method to clear the textboxes
     public void clearTxt() {
         txtReportedOn.setText("");
-        txtID.setText(""); //little hack to trick it into taking the int as String
+        txtID.setText("");
         txtResolved.setText("");
         txtResolvedOn.setText("");
     }
